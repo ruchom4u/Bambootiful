37bfa822a7735f33798014bed8a995dcd14584b9
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2019-12-21 10:40:44 -0600

    Begun development

--- src/main/java/com/bagel/bamboozled/Bamboozled.java
+++ src/main/java/com/bagel/bamboozled/Bamboozled.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.bagel.bamboozled;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -19,13 +19,13 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod("bamboozled")
+public class Bamboozled
 {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public Bamboozled() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
